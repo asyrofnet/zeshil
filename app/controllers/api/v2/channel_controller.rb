b9534bb2990be8cc539d1613c4a590086dd318d8
taufan <hadikusumaa15@gmail.com>
2019-05-29 10:55:12 +0700

    add username on sidebar profile

--- app/controllers/api/v2/channel_controller.rb
+++ app/controllers/api/v2/channel_controller.rb
@@ -2,33 +2,6 @@ require 'jwt'
 require 'uri'
 
 class Api::V2::ChannelController < ApplicationController
-
-  def generate_invite_url
-    begin
-      # params[:username] = "channel_qiscus"
-      invite_url = "kiwari.me/?username=#{params[:username]}"
-      message = "invite url anda :\n#{invite_url}"
-
-      topic_id =  2456640
-      qiscus_token = "D4cAi4F0NctKh8ljtko5"
-      app_id = "kiwari-stag"
-      qiscus_sdk_secret = "kiwari-stag-123"
-
-      qiscus_sdk = QiscusSdk.new(app_id, qiscus_sdk_secret)
-      comments = qiscus_sdk.post_comment(qiscus_token, topic_id, message, "text")
-
-      render json: {
-        message: comments
-      }
-    rescue Exception => e
-      render json: {
-        error: {
-          message: e.message
-        }
-      }, status: 422 and return
-    end
-  end
-
   def show
     begin
       success = false
@@ -61,7 +34,6 @@ class Api::V2::ChannelController < ApplicationController
               p "chat room found"
               room_id = chat_room.qiscus_room_id
               if @os == "android"
-                # location = "https://play.google.com/store/apps/details?id=com.qiscus.kiwari&referrer=#{room_id}"
                 bundle_id = ENV['ANDROID_BUNDLE_ID'] || "com.qiscus.kiwari"
                 location = "market://details?id=#{bundle_id}&referrer=#{room_id}"
               elsif @os == "ios"
