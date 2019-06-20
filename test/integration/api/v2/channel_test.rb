71ff81b43ef456cfea186e82de72f132bccbdd24
asyrof <asyrofqomaruddin@gmail.com>
2019-06-20 13:14:50 +0700

    small check for channel test

--- test/integration/api/v2/channel_test.rb
+++ test/integration/api/v2/channel_test.rb
@@ -21,6 +21,24 @@ class API::V2::ChannelTest< ActionDispatch::IntegrationTest
     assert_equal message, response_data["message"]
   end
 
+  test "user1 will not get random channel" do
+    user1 = users(:user1)
+    session1 = auth_sessions(:user1_session1)
+    message = "channel random tidak ditemukan"
+    
+
+    
+    get "/api/v2/channel/username_to_room_id",
+      params: {:username=> "random"},
+      headers: { 'Authorization' => token_header(session1.jwt_token) }
+    assert_equal 200, response.status
+    assert_equal Mime[:json], response.content_type
+    
+    response_data = JSON.parse(response.body)
+    assert_equal false, response_data["success"]
+    assert_equal message, response_data["message"]
+  end
+
   test "unauthorized will get error" do
    
 
