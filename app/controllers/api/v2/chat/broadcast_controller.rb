63c83b145b7b17949837edc13970b6cacda3459f
asyrof <asyrofqomaruddin@gmail.com>
2019-06-28 11:13:40 +0700

    change api broadcast use phone number

--- app/controllers/api/v2/chat/broadcast_controller.rb
+++ app/controllers/api/v2/chat/broadcast_controller.rb
@@ -10,16 +10,34 @@ class Api::V2::Chat::BroadcastController < ProtectedController
     #
     # @apiParam {String} access_token User access token
     # @apiParam {String} message The Message for Targets
-    # @apiParam {Array} target_qiscus_emails the emails of targets in array form 
+    # @apiParam {Array} phone_number[] Array of normalized phone number, for instance: `phone_number[]=+62...&phone_number[]=+62...`
     # @apiParam {String} type The type of comment, default is text. optional 
     # @apiParam {JsonObject} payload payload needed for type other than text 
     # =end
     def send_broadcast
       begin
-        target_qiscus_emails = params[:target_qiscus_emails]
-        if target_qiscus_emails.nil? || !target_qiscus_emails.kind_of?(Array)
-          raise Exception.new("Target qiscus emails must be present in array form.")
+        phone_number = params[:phone_number]
+        if phone_number.nil? || !phone_number.kind_of?(Array)
+          raise Exception.new("phone_number must be present in array form.")
         end
+        phone_numbers = Array.new
+        current_user_phone_number = @current_user.phone_number
+          
+          params[:phone_number].each do | phone_number |
+  
+            phone_number = phone_number.strip().delete(' ') # remove all spaces
+                      phone_number = phone_number.gsub(/[[:space:]]/, '')
+  
+            if phone_number.start_with?("8")
+              phone_number = @current_user.country_code + phone_number
+            elsif phone_number.start_with?("0")
+              phone_number = phone_number[1..-1]
+              phone_number = @current_user.country_code + phone_number
+            end
+             phone_numbers.push(phone_number)
+          end
+
+        target_qiscus_emails = User.where(phone_number: phone_numbers).pluck(:qiscus_email)
         type_text = "text"
         type = params[:type] || type_text
         message = params[:message]
