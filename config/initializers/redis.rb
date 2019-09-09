0623b851580551ecacf44b98c30925a7b3d058ab
iskandarsuhaimi <ijoiskandar@gmail.com>
2019-09-09 14:21:30 +0700

    update redis host

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if ENV["REDIS_URL"] == "redis-secret-stag"
-  $redis = Redis.new(:url => "redis://:UD4ywLh7V13N4bNe@redis-persistent-x77cx:6379/0") 
+  $redis = Redis.new(:url => "redis://:UD4ywLh7V13N4bNe@redis-persistent-x77cx:6379") 
 else
   $redis = Redis.new(:url => ENV["REDIS_URL"]) #default
 end
