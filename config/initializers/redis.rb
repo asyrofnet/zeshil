8b308e353b530efbbe1270bcf6d4bc41e6b8c715
iskandarsuhaimi <ijoiskandar@gmail.com>
2019-09-10 09:54:43 +0700

    fix redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,3 @@
-if ENV["REDIS_URL"] == "redis-secret-stag"
-  $redis = Redis.new(:url => "redis://:UD4ywLh7V13N4bNe@redis-persistent-x77cx:6379") 
-else
+if ENV["REDIS_URL"]
   $redis = Redis.new(:url => ENV["REDIS_URL"]) #default
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,9 @@
 Sidekiq.configure_server do |config|
-  if ENV["REDIS_URL"] == "redis-secret-stag"
-    url = "redis://:UD4ywLh7V13N4bNe@redis-persistent-x77cx:6379")
-    config.redis = { url: url, network_timeout: 5}
-  else
-    config.redis = { url: ENV['REDIS_URL'], network_timeout: 5}
-  end
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5}
 end
 
 Sidekiq.configure_client do |config|
-  if ENV["REDIS_URL"] == "redis-secret-stag"
-    url = "redis://:UD4ywLh7V13N4bNe@redis-persistent-x77cx:6379")
-    config.redis = { url: url, size: 3 }
-  else
-    config.redis = { url: ENV['REDIS_URL'], size: 3 }
-  end
+  config.redis = { url: ENV['REDIS_URL'], size: 3 }
 end
 
 # https://github.com/mperham/sidekiq/wiki/Advanced-Options
